fc585e07eaa650441c2dffdd29758a15cfa10bfb
guidojw <35309288+guidojw@users.noreply.github.com>
2023-03-17 15:56:40 +0100

    feat(GuildMemberUpdateEvent): implement job running on premium/displayName change

--- src/client/events/guild-member-update.ts
+++ src/client/events/guild-member-update.ts
@@ -1,14 +1,27 @@
+import { inject, injectable, named } from 'inversify'
 import type { BaseHandler } from '..'
+import { BaseJob } from '../../jobs'
 import type { GuildMember } from 'discord.js'
-import { injectable } from 'inversify'
+import { constants } from '../../utils'
+
+const { TYPES } = constants
 
 @injectable()
 export default class GuildMemberUpdateEventHandler implements BaseHandler {
+  @inject(TYPES.Job)
+  @named('updatePlaytesters')
+  private readonly updatePlaytestersJob!: BaseJob
+
   public async handle (oldMember: GuildMember, newMember: GuildMember): Promise<void> {
-    if (oldMember.premiumSince === null && newMember.premiumSince !== null) {
-
-    } else if (oldMember.premiumSince !== null && newMember.premiumSince === null) {
-
+    if (
+      oldMember.premiumSince !== newMember.premiumSince ||
+      (newMember.premiumSince !== null && oldMember.displayName !== newMember.displayName)
+    ) {
+      await this.updatePlaytestersJob.run(
+        parseInt(process.env.UNIVERSE_ID as string),
+        process.env.GUILD_ID,
+        process.env.OUTPUT_CHANNEL_ID
+      )
     }
   }
 }
